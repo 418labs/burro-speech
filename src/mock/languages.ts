921e35c5982a4af17d1b7cc604ff900f084c7250
Jona <llamasjonat@gmail.com>
2025-03-09 07:14:10 -0300

    feat: added mock languages

---
+++ src/mock/languages.ts
@@ -0,0 +1,12 @@
+export const MOCK_LANGUAGES = [
+  { code: 'en-US', name: 'EN' },
+  { code: 'es-AR', name: 'ES' },
+  { code: 'fr-FR', name: 'FR' },
+  { code: 'de-DE', name: 'DE' },
+  { code: 'it-IT', name: 'IT' },
+  { code: 'pt-BR', name: 'PT' },
+  { code: 'ja-JP', name: 'JA' },
+  { code: 'zh-CN', name: 'ZH' },
+  { code: 'ru-RU', name: 'RU' },
+  { code: 'ar-SA', name: 'AR' },
+];
