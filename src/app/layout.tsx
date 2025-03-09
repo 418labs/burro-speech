0d079afb9663bc576e75d24f5a1bd203023d0cb7
Jona <llamasjonat@gmail.com>
2025-03-09 20:46:05 -0300

    chore: integrate Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 import { siteConfig } from '@/config/site';
 
 import './globals.css';
+
+const GOOGLE_ANALYTICS = process.env.GOOGLE_ANALYTICS || '';
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || ''),
@@ -43,6 +46,7 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         <div className='relative overflow-x-hidden flex flex-col w-screen min-h-screen bg-background'>{children}</div>
         <Analytics />
+        <GoogleAnalytics gaId={GOOGLE_ANALYTICS} />
       </body>
     </html>
   );
